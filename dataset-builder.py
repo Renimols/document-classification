59351a53f92ef3ed292b6efc8f0d68cd5f0d35fc
Renimols <renimol2013@gmail.com>
2021-08-14 12:30:43 +0530

    formatted files

--- dataset-builder.py
+++ dataset-builder.py
@@ -3,10 +3,10 @@ import os
 import re
 
 directory = '/home/archana/Desktop/reni/bbc/'
-with open("/home/archana/Desktop/test_data.csv", "w") as csv_file:   
+with open("/home/archana/Desktop/test_data.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["documents","categories"]) 
-    for foldername in os.listdir(directory) :
+    writer.writerow(["documents", "categories"])
+    for foldername in os.listdir(directory):
         for filename in os.scandir(''.join(directory+foldername)):
-            with open(filename.path,encoding="utf8", errors='ignore') as file:
-                writer.writerow((file.readlines(), foldername)) 
+            with open(filename.path, encoding="utf8", errors='ignore') as file:
+                writer.writerow((file.readlines(), foldername))
